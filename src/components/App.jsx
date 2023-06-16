0c246424055535de39234b0a4fa578465ff66d2b
unknown <ivan.ruskevych@gmail.com>
2023-06-16 04:30:12 +0300

    step-02 fetchContacts & addContacts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,18 @@
+import { useDispatch } from 'react-redux';
 import ContactForm from './ContactForm/ContactForm';
 import ContactsList from './ContactsList/ContactsList';
 import Filter from './Filter/Filter';
 import Section from './Section/Section ';
+import { useEffect } from 'react';
+import { fetchContacts } from 'redux/operations';
 
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
     <Section>
       <h1>Phonebook</h1>
